d2a4f8b0303ab60085c282102aa89102ed7a4688
alex-379 <alexey_379@mail.ru>
2024-06-19 15:40:43 +1000

    fix (reconnection to db)

--- DataSeeder.Core/Database/CrmContext.cs
+++ DataSeeder.Core/Database/CrmContext.cs
@@ -3,47 +3,75 @@ using DataSeeder.Core.Models;
 using Microsoft.EntityFrameworkCore;
 using Npgsql;
 
-namespace DataSeeder.Core.Database;
-
-public class CrmContext : DbContext
+namespace DataSeeder.Core.Database
 {
-    private const string database = "CrmLocalDb_ENVIRONMENT";
-    public virtual DbSet<Lead> Leads { get; init; } = default;
-    public virtual DbSet<Account> Accounts { get; init; } = default;
-    
-    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    public class CrmContext : DbContext
     {
-        options.UseNpgsql(ConfigureDataSource());
-        options.UseSnakeCaseNamingConvention();
-        options.EnableSensitiveDataLogging();
-    }
+        private const string database = "CrmDb_ENVIRONMENT";
 
-    private static NpgsqlDataSource ConfigureDataSource()
-    {
-        var dataSourceBuilder = new NpgsqlDataSourceBuilder(Environment.GetEnvironmentVariable(database));
-        dataSourceBuilder.MapEnum<AccountStatus>();
-        dataSourceBuilder.MapEnum<Currency>();
-        dataSourceBuilder.MapEnum<LeadStatus>();
-        var dataSource = dataSourceBuilder.Build();
+        public virtual DbSet<Lead> Leads { get; init; } = default;
+        public virtual DbSet<Account> Accounts { get; init; } = default;
 
-        return dataSource;
-    }
-    
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        modelBuilder.Entity<Lead>().Property(m => m.Name)
-            .HasMaxLength(100);
-        modelBuilder.Entity<Lead>().Property(m => m.Mail)
-            .HasMaxLength(100);
-        modelBuilder.Entity<Lead>().Property(m => m.Phone)
-            .HasMaxLength(100);
-        modelBuilder.Entity<Lead>().Property(m => m.Address)
-            .HasMaxLength(100);
-        modelBuilder.Entity<Lead>().Property(m => m.Password)
-            .HasMaxLength(100);
-        modelBuilder.Entity<Lead>().Property(m => m.Salt)
-            .HasMaxLength(100);
-        modelBuilder.Entity<Lead>().Property(m => m.RefreshToken)
-            .HasMaxLength(100);
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            var dataSource = ConfigureDataSource();
+            options.UseNpgsql(dataSource);
+            options.UseSnakeCaseNamingConvention();
+        }
+
+        private static NpgsqlDataSource ConfigureDataSource()
+        {
+            var dataSourceBuilder = new NpgsqlDataSourceBuilder(Environment.GetEnvironmentVariable(database));
+            dataSourceBuilder.MapEnum<AccountStatus>();
+            dataSourceBuilder.MapEnum<Currency>();
+            dataSourceBuilder.MapEnum<LeadStatus>();
+            var dataSource = dataSourceBuilder.Build();
+
+            return dataSource;
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Lead>().Property(m => m.Name)
+                .HasMaxLength(100);
+            modelBuilder.Entity<Lead>().Property(m => m.Mail)
+                .HasMaxLength(100);
+            modelBuilder.Entity<Lead>().Property(m => m.Phone)
+                .HasMaxLength(100);
+            modelBuilder.Entity<Lead>().Property(m => m.Address)
+                .HasMaxLength(100);
+            modelBuilder.Entity<Lead>().Property(m => m.Password)
+                .HasMaxLength(100);
+            modelBuilder.Entity<Lead>().Property(m => m.Salt)
+                .HasMaxLength(100);
+            modelBuilder.Entity<Lead>().Property(m => m.RefreshToken)
+                .HasMaxLength(100);
+        }
+
+        public static async Task ExecuteWithRetryAsync(Func<CancellationToken, Task> action, CancellationToken cancellationToken = default)
+        {
+            int retryCount = 0;
+            const int maxRetryCount = 5;
+            const int initialRetryDelay = 500;
+
+            while (retryCount < maxRetryCount)
+            {
+                try
+                {
+                    await action(cancellationToken);
+                    return;
+                }
+                catch (Exception)
+                {
+                    retryCount++;
+                    int retryDelay = (int)Math.Pow(2, retryCount - 1) * initialRetryDelay;
+
+                    Console.WriteLine($"Database connection error. Retry #{retryCount} after {retryDelay} ms.");
+                    await Task.Delay(retryDelay, cancellationToken);
+                }
+            }
+
+            throw new Exception("Failed to connect to the database after several attempts.");
+        }
     }
 }
--- DataSeeder.DataUpdater/Program.cs
+++ DataSeeder.DataUpdater/Program.cs
@@ -9,6 +9,9 @@ namespace DataSeeder.DataUpdater;
 
 public static class Program
 {
+    private const int totalLeads = 4000000;
+    private const int pageSize = 10000;
+    private const int totalPages = totalLeads/pageSize;
     private static readonly Currency[] _allowedCurrenciesForRegularLead = [Currency.Rub, Currency.Usd, Currency.Eur];
     private static readonly Currency[] _allowedCurrenciesForVipLead = ((Currency[])Enum.GetValues(typeof(Currency)))
         .Where(c => c != Currency.Unknown)
@@ -16,55 +19,60 @@ public static class Program
 
     public static async Task Main()
     {
-        const int pageSize = 100000;
-        const int totalPages = 40;
         var counter = 0;
 
         var accounts = new List<Account>();
         await using var context = new CrmContext();
-        for (var pageIndex = 0; pageIndex < totalPages; pageIndex++)
+        var testLead = await context.Leads.Include(l => l.Accounts).FirstOrDefaultAsync();
+        await CrmContext.ExecuteWithRetryAsync(async (cancellationToken) =>
         {
-            var leads = await context.Leads
-                .Include(l => l.Accounts)
-                .Skip(pageSize * pageIndex)
-                .Take(pageSize)
-                .ToListAsync();
-
-            if (!ValidationAccounts.CheckAccounts(leads))
+            for (var pageIndex = 0; pageIndex < totalPages; pageIndex++)
             {
-                foreach (var lead in leads)
+                var leads = await context.Leads
+                    .Include(l => l.Accounts)
+                    .Skip(pageSize * pageIndex)
+                    .Take(pageSize)
+                    .ToListAsync(cancellationToken: cancellationToken);
+
+                if (!ValidationAccounts.CheckAccounts(leads))
                 {
-                    if (lead.Accounts.Count == 0)
+                    foreach (var lead in leads)
                     {
-                        var account = new Account
+                        if (lead.Accounts.Count == 0)
                         {
-                            Id = Guid.NewGuid(),
-                            Currency = Currency.Rub,
-                            LeadId = lead.Id,
-                            Status = AccountStatus.Active
-                        };
-                        context.Accounts.Add(account);
-                        await context.SaveChangesAsync();
+                            var account = new Account
+                            {
+                                Id = Guid.NewGuid(),
+                                Currency = Currency.Rub,
+                                LeadId = lead.Id,
+                                Status = AccountStatus.Active
+                            };
+                            context.Accounts.Add(account);
+                            await context.SaveChangesAsync(cancellationToken);
+                        }
+
+                        lead.Accounts = lead.Status != LeadStatus.Vip
+                            ? DataUpdater.UpdateAccountsForLead(lead, _allowedCurrenciesForRegularLead)
+                            : DataUpdater.UpdateAccountsForLead(lead, _allowedCurrenciesForVipLead);
+                        accounts.AddRange(lead.Accounts);
                     }
-
-                    lead.Accounts = lead.Status != LeadStatus.Vip
-                        ? DataUpdater.UpdateAccountsForLead(lead, _allowedCurrenciesForRegularLead)
-                        : DataUpdater.UpdateAccountsForLead(lead, _allowedCurrenciesForVipLead);
-                    accounts.AddRange(lead.Accounts);
                 }
-            }
 
-            if (!ValidationAccounts.CheckAccounts(leads))
-            {
-                throw new ValidationException();
-            }
-            
-            context.Accounts.UpdateRange(accounts);
-            await context.SaveChangesAsync();
+                if (!ValidationAccounts.CheckAccounts(leads))
+                {
+                    throw new ValidationException();
+                }
 
-            accounts.Clear();
-            Console.WriteLine($"Iteration {counter} on {pageSize} leads");
-            counter++;
-        }
+                context.Accounts.UpdateRange(accounts);
+                await context.SaveChangesAsync(cancellationToken);
+
+                accounts.Clear();
+                Console.WriteLine($"Iteration {counter} on {pageSize} leads");
+                counter++;
+            }
+        });
+
+
+        
     }
 }
